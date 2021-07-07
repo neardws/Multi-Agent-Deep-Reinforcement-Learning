e34808999c7d9a3a525928c6d38a19d9492ad1e7
Near <singleangel@vip.qq.com>
2021-07-07 22:14:15 +0800

    Add Experience and Reward Replay Buffer

---
+++ Utilities/Data_structures/Reward_Replay_Buffer.py
@@ -0,0 +1,101 @@
+# -*- coding: UTF-8 -*-
+"""
+@Project ：Hierarchical-Reinforcement-Learning 
+@File    ：Reward_Replay_Buffer.py
+@Author  ：Neardws
+@Date    ：7/7/21 7:35 下午 
+"""
+from collections import namedtuple, deque
+import random
+import torch
+import numpy as np
+
+
+class Reward_Replay_Buffer(object):
+    """Replay buffer to store past reward experiences that the agent can then use for training data"""
+
+    def __init__(self, buffer_size, batch_size, seed, device=None):
+        """
+        Init Replay_buffer
+        :param buffer_size: buffer size
+        :param batch_size: batch number
+        :param seed: seed of random number
+        :param device: GPU or CPU
+        """
+        self.memory = deque(maxlen=buffer_size)
+        self.batch_size = batch_size
+        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "next_state", "done"])
+        random.seed(seed)  # setup random number seed
+        # if the device is not settle, then use available GPU, if not, the cpu
+        if device:
+            self.device = torch.device(device)
+        else:
+            self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    def add_experience(self, states, actions, rewards, next_states, dones):
+        """
+        Adds experience(s) into the replay buffer
+        :param states: state of agent
+        :param actions: action of agent
+        :param rewards: reward got after take the action
+        :param next_states: the next state after take the action
+        :param dones: the episode is over or not
+        :return: None
+        """
+        if type(dones) == list:
+            assert type(dones[0]) != list, "A done shouldn't be a list"
+            experiences = [self.experience(state, action, reward, next_state, done)
+                           for state, action, reward, next_state, done in
+                           zip(states, actions, rewards, next_states, dones)]
+            self.memory.extend(experiences)
+        else:
+            experience = self.experience(states, actions, rewards, next_states, dones)
+            self.memory.append(experience)
+
+    def sample(self, num_experiences=None, separate_out_data_types=True):
+        """
+        Draws a random sample of experience from the replay buffer
+        :param num_experiences: the number of experience
+        :param separate_out_data_types: True or False, indicate is the return separate
+        :return:
+        """
+        experiences = self.pick_experiences(num_experiences)
+        if separate_out_data_types:
+            states, actions, rewards, next_states, dones = self.separate_out_data_types(experiences)
+            return states, actions, rewards, next_states, dones
+        else:
+            return experiences
+
+    def separate_out_data_types(self, experiences):
+        """
+        Puts the sampled experience into the correct format for a PyTorch neural network
+        :param experiences: Input
+        :return:/
+        """
+        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(self.device)
+        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).float().to(self.device)
+        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(self.device)
+        next_states = torch.from_numpy(np.vstack([e.next_state for e in experiences if e is not None])).float().to(
+            self.device)
+        dones = torch.from_numpy(np.vstack([int(e.done) for e in experiences if e is not None])).float().to(self.device)
+
+        return states, actions, rewards, next_states, dones
+
+    def pick_experiences(self, num_experiences=None):
+        """
+        random pick experience from memory
+        :param num_experiences: the number of experiences
+        :return: random samples
+        """
+        if num_experiences is not None:
+            batch_size = num_experiences
+        else:
+            batch_size = self.batch_size
+        return random.sample(self.memory, k=batch_size)
+
+    def __len__(self):
+        """
+        The length of Replay_Buffer equal to the length of memory, i.e., buffer_size
+        :return: length of Replay_Buffer
+        """
+        return len(self.memory)
